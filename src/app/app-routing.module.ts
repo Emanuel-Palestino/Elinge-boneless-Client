18945b0ae035ed8c2afa396c3a5e7e5740532949
Emanuel <emanuel.palestino.h@gmail.com>
2022-01-20 12:09:52 -0600

    Barra de navegación creada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarraNavegacionComponent } from './components/barra-navegacion/barra-navegacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "navegacion",
+    component: BarraNavegacionComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './components/barra-navegacion/barra-navegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionComponent,
   ],
   imports: [
     BrowserModule,
