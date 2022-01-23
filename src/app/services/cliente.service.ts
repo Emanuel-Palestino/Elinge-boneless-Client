9dc367013e02fe07edb86243b641bd97adcf0889
Rugal <scaryskriller@gmail.com>
2022-01-23 14:58:08 -0600

    actualizacion cliente service

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient } from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  registrarCliente(cliente: ClienteRegistro , direccion: Direccion){
+    //recibir lo ingresado en el formulario 
+    //llamado a la api de registrar cliente pasar datos del modelo cliente, regresa datos en JSON
+    //agregar a modelo de direccion 
+    //enviar api de registrar direccion
+  }
 }
