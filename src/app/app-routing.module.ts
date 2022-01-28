d295a737e842091a375ac2fd0160f1daef382c1c
homs Vázquez <dyese@Hassam.VASQUEZ>
2022-01-28 00:16:50 -0600

    Login Admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './components/clientes/clientes.component';
+import { InicioSesionAdministradorComponent } from './components/inicio-sesion-administrador/inicio-sesion-administrador.component';
 import { InicioSesionComponent } from './components/inicio-sesion/inicio-sesion.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { ResumenComponent } from './components/resumen/resumen.component';
-
 const routes: Routes = [
   {
     path: "",
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: "registrarse",
     component: RegistroComponent
+  },
+  {
+    path: "administracion/ingresar",
+    component: InicioSesionAdministradorComponent
   },
   {
     path: "app",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ClientesComponent } from './components/clientes/clientes.component';
 import { ResumenComponent } from './components/resumen/resumen.component';
+import { InicioSesionAdministradorComponent } from './components/inicio-sesion-administrador/inicio-sesion-administrador.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { ResumenComponent } from './components/resumen/resumen.component';
     PieDePaginaComponent,
     ClientesComponent,
     ResumenComponent,
+    InicioSesionAdministradorComponent,
   ],
   imports: [
     BrowserModule,
