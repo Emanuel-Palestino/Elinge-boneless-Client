8f9773361b0cddcbe1b88b1351b252568a1d698d
Avelitro <joseabelmartinezleyva@gmail.com>
2022-01-30 22:41:20 -0600

    Servicio AgregarNuevaDireccion

--- src/app/components/direcciones-agregar/direcciones-agregar.component.ts
+++ src/app/components/direcciones-agregar/direcciones-agregar.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { DireccionesService } from '../../services/direcciones.service';
+import { DireccionNueva } from '../../models/DireccionNueva.model';
 
 @Component({
   selector: 'app-direcciones-agregar',
   templateUrl: './direcciones-agregar.component.html',
   styleUrls: ['./direcciones-agregar.component.css']
 })
-export class DireccionesAgregarComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class DireccionesAgregarComponent implements OnInit
+{
+  direccionNueva:DireccionNueva;
+  constructor(private direccionesServicie:DireccionesService) 
+  {
+    this.direccionNueva=new DireccionNueva();
+  }
+  ngOnInit(): void 
+  {}
+  //Metodo nuevo 
+  agregarDireccion():void
+  {
+    console.log("Entrando para agregar una direccion nueva")
+    let idCliente = localStorage.getItem('idCliente');
+    this.direccionesServicie.crearNuevaDireccion(Number(idCliente),this.direccionNueva).then(res => { 
+      console.log("Direccion Agregada")
+    }, err => console.error(err) );
   }
 
 }
